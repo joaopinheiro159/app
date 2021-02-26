cea73f648cf247985a3d4baeb238e01dd0b88d20
Fábio Jansen <fabiojansen@outlook.com>
2021-02-26 09:08:41 -0300

    feat: add main modules

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,7 +1,17 @@
 import 'package:get/get.dart';
 
+import 'package:klinimed_app/app/modules/forgot_password/bindings/forgot_password_binding.dart';
+import 'package:klinimed_app/app/modules/forgot_password/views/forgot_password_view.dart';
 import 'package:klinimed_app/app/modules/home/bindings/home_binding.dart';
 import 'package:klinimed_app/app/modules/home/views/home_view.dart';
+import 'package:klinimed_app/app/modules/login/bindings/login_binding.dart';
+import 'package:klinimed_app/app/modules/login/views/login_view.dart';
+import 'package:klinimed_app/app/modules/menu/bindings/menu_binding.dart';
+import 'package:klinimed_app/app/modules/menu/views/menu_view.dart';
+import 'package:klinimed_app/app/modules/register/bindings/register_binding.dart';
+import 'package:klinimed_app/app/modules/register/views/register_view.dart';
+import 'package:klinimed_app/app/modules/splash/bindings/splash_binding.dart';
+import 'package:klinimed_app/app/modules/splash/views/splash_view.dart';
 
 part 'app_routes.dart';
 
@@ -13,6 +23,31 @@ class AppPages {
       name: _Paths.HOME,
       page: () => HomeView(),
       binding: HomeBinding(),
+    ),
+    GetPage(
+      name: _Paths.SPLASH,
+      page: () => SplashView(),
+      binding: SplashBinding(),
+    ),
+    GetPage(
+      name: _Paths.LOGIN,
+      page: () => LoginView(),
+      binding: LoginBinding(),
+    ),
+    GetPage(
+      name: _Paths.REGISTER,
+      page: () => RegisterView(),
+      binding: RegisterBinding(),
+    ),
+    GetPage(
+      name: _Paths.FORGOT_PASSWORD,
+      page: () => ForgotPasswordView(),
+      binding: ForgotPasswordBinding(),
+    ),
+    GetPage(
+      name: _Paths.MENU,
+      page: () => MenuView(),
+      binding: MenuBinding(),
     ),
   ];
 }
--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -3,8 +3,18 @@ part of 'app_pages.dart';
 
 abstract class Routes {
   static const HOME = _Paths.HOME;
+  static const SPLASH = _Paths.SPLASH;
+  static const LOGIN = _Paths.LOGIN;
+  static const REGISTER = _Paths.REGISTER;
+  static const FORGOT_PASSWORD = _Paths.FORGOT_PASSWORD;
+  static const MENU = _Paths.MENU;
 }
 
 abstract class _Paths {
   static const HOME = '/home';
+  static const SPLASH = '/splash';
+  static const LOGIN = '/login';
+  static const REGISTER = '/register';
+  static const FORGOT_PASSWORD = '/forgot-password';
+  static const MENU = '/menu';
 }
